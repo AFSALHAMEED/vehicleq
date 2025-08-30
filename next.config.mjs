a5bbdef60dcd4ba1de2fb8b9bdef8492d96274db
afsalhameed <106297818+AFSALHAMEED@users.noreply.github.com>
2025-08-30 18:06:41 +0530

    Update next.config.mjs
    new host name added

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "vbhreumhadzgssngpnon.supabase.co",
+        hostname: "oxqktnuqlecqgrzdkdva.supabase.co",
       },
     ],
   },
